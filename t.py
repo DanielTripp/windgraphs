9367a4085deb2e890f4f2883293d9f77e7008c42
danieltripp <danieltripp@localhost>
2016-07-16 13:25:29 -0400

    .

--- t.py
+++ t.py
@@ -2,13 +2,20 @@
 
 import urllib2, json, pprint
 
-url = 'http://widget.windguru.cz/int/widget_json.php?s=64&m=3&lng=en'
-resp = urllib2.urlopen(url).read()
-resp = resp.strip('(').strip(')')
+if 1:
+	url = 'http://widget.windguru.cz/int/widget_json.php?s=64&m=3&lng=en'
+	resp = urllib2.urlopen(url).read()
+	resp = resp.strip('(').strip(')')
+	#with open('d-test-predictions', 'w') as fout:
+	#	fout.write(resp)
+else:
+	with open('d-test-predictions') as fin:
+		resp = fin.read()
 data = json.loads(resp)
-pprint.pprint(data)
-
-
+d2 = data['fcst']['fcst']['3']
+windspeed = d2['WINDSPD']
+windgusts = d2['GUST']
+pprint.pprint(d2)
 
 
 
