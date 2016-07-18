f9da9510c938b8e26c44a88b817b43187f7159fb
danieltripp <danieltripp@localhost>
2016-07-17 20:35:44 -0400

    .

--- t.py
+++ t.py
@@ -2,20 +2,37 @@
 
 import urllib2, json, pprint
 
-if 1:
-	url = 'http://widget.windguru.cz/int/widget_json.php?s=64&m=3&lng=en'
-	resp = urllib2.urlopen(url).read()
-	resp = resp.strip('(').strip(')')
-	#with open('d-test-predictions', 'w') as fout:
-	#	fout.write(resp)
-else:
-	with open('d-test-predictions') as fin:
-		resp = fin.read()
-data = json.loads(resp)
-d2 = data['fcst']['fcst']['3']
-windspeed = d2['WINDSPD']
-windgusts = d2['GUST']
-pprint.pprint(d2)
+def wg_get_web_response():
+	if 0:
+		url = 'http://widget.windguru.cz/int/widget_json.php?s=64&m=3&lng=en'
+		r = urllib2.urlopen(url).read()
+		r = resp.strip('(').strip(')')
+		if 0:
+			with open('d-test-predictions-3', 'w') as fout:
+				fout.write(r)
+	else:
+		with open('d-test-predictions-3') as fin:
+			r = fin.read()
+	return r
 
+def wg_get_forecast_windspeed_from_web_response(web_response_str_, day_of_week_, hour_):
+	parsed_response = json.loads(web_response_str_)
+	data = parsed_response['fcst']['fcst']['3']
+	windspeed = data['WINDSPD']
+	windgusts = data['GUST']
+	days_of_week = data['hr_weekday']
+	hours = data['hr_h']
+	for i, (day_of_week, hour) in enumerate(zip(days_of_week, hours)):
+		if day_of_week == day_of_week_ and hour == hour_:
+			break
+	else:
+		raise Exception('day/hour not found')
+	return windspeed[i]
+	#pprint.pprint(days_of_week)
 
+def wg_get_forecast_windspeed(day_of_week_, hour_):
+	web_response = wg_get_web_response()
+	return wg_get_forecast_windspeed_from_web_response(web_response, day_of_week_, hour_)
+
+print wg_get_forecast_windspeed(1, '11')
 
