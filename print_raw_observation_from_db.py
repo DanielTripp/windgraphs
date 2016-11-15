b00f6b98a98fd8db624c1c00154fd11fdebf2b56
danieltripp <danieltripp@localhost>
2016-11-14 23:14:55 -0500

    Rough work.  Broken.

--- print_raw_observation_from_db.py
+++ print_raw_observation_from_db.py
@@ -4,8 +4,13 @@ import sys
 from misc import *
 import windgraphs
 
+def main():
+	channel = sys.argv[1]
+	datestr = sys.argv[2]
+	windgraphs.print_raw_observation_from_db(channel, datestr)
+
 if __name__ == '__main__':
 
-	datestr = sys.argv[1]
-	windgraphs.print_raw_observation_from_db(datestr)
+	main()
+
 
--- print_reparsed_observation_from_db.py
+++ print_reparsed_observation_from_db.py
@@ -3,11 +3,13 @@
 import sys
 import windgraphs
 
-if __name__ == '__main__':
-
+def main():
 	channel = sys.argv[1]
 	datestr = sys.argv[2]
 	windgraphs.print_reparsed_observation_from_db(channel, datestr)
 
+if __name__ == '__main__':
+
+	main()
 
 
