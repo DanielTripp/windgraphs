3335570bb428e1944eb2a3b023eb38c8bcf31904
danieltripp <danieltripp@localhost>
2016-12-01 23:12:05 -0500

    For developers - backfill_reparse_raw_forecast_in_db.py utility - can now take a timestamp range (instead of a single timestamp.)

--- backfill_reparse_raw_forecast_in_db.py
+++ backfill_reparse_raw_forecast_in_db.py
@@ -2,10 +2,20 @@
 
 import sys
 import windgraphs
+from misc import *
 
 if __name__ == '__main__':
 
 	weather_channel = sys.argv[1]
-	datestr = sys.argv[2]
-	windgraphs.backfill_reparse_raw_forecast_in_db(weather_channel, datestr)
+	if len(sys.argv) == 3:
+		datestr = sys.argv[2]
+		windgraphs.backfill_reparse_raw_forecast_in_db(weather_channel, datestr)
+	elif len(sys.argv) == 4:
+		start_datestr_incl = sys.argv[2]
+		end_datestr_incl = sys.argv[3]
+		for epoch_time in range(str_to_em(start_datestr_incl), str_to_em(end_datestr_incl), 1000*60*60):
+			datestr = em_to_str(epoch_time)
+			windgraphs.backfill_reparse_raw_forecast_in_db(weather_channel, datestr)
+	else:
+		raise Exception()
 
