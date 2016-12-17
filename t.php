7dad2c6256ba45b3df38188a3be9cebd101a24d1
danieltripp <danieltripp@localhost>
2016-12-16 23:20:54 -0500

    .

---
+++ t.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <script src="jquery-3.1.0.min.js"></script>
+  <script type="text/javascript">
+
+function initialize() {
+<?php 
+	if(false) {
+?>
+	console.log('false');
+<?php 
+	} else {
+?>
+	console.log('true');
+<?php 
+	}
+?>
+}
+
+$(document).ready(initialize);
+
+  </script>
+  </head>
+  <body>
+    <h2>Wind forecasts vs. actual wind - Toronto Islands</h2>
+    <br>
+    test
+  </body>
+</html>
