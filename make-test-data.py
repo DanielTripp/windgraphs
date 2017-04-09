e0815fcf5165909db1e55f41591ad33e51a267d8
danieltripp <danieltripp@localhost>
2017-04-09 13:58:41 -0400

    .

--- make-test-data.py
+++ make-test-data.py
@@ -6,17 +6,27 @@ import windgraphs
 
 if __name__ == '__main__':
 
-	if 0:
+	print "delete from wind_observations_parsed where time_retrieved < str_to_em('1981');"
+	print "delete from wind_forecasts_parsed where time_retrieved < str_to_em('1981');"
+
+	if 1:
 		for day in xrange(1, 30):
-			print "insert into wind_observations_parsed values ('gc.ca', str_to_em('1980-01-%02d 08:00:00'), '1980-01-%02d 08:00:00', 10, -1);" % (day, day)
+			wind = max(0, get_range_val((16,0), (17,1), day))
+			print "insert into wind_observations_parsed values ('gc.ca', str_to_em('1980-01-%02d 08:00:00'), '1980-01-%02d 08:00:00', %d, -1);" \
+					% (day, day, wind)
 
 	if 1:
 		for day in xrange(1, 30):
 			for channel in windgraphs.PARSED_WEATHER_CHANNELS:
 				time_retrieved_str = '1980-01-%02d 06:00:00' % day
 				target_time_str = '1980-01-%02d 08:00:00' % day
-				print "insert into wind_forecasts_parsed values (%10s, %d, '%s', %d, '%s', 10, -1);" % \
-						("'%s'" % channel, str_to_em(time_retrieved_str), time_retrieved_str, str_to_em(target_time_str), target_time_str)
+				obs_wind = max(0, get_range_val((16,0), (17,1), day))
+				num_forecast_winds = max(1, get_range_val((16,1), (17,2), day))
+				forecast_winds = [obs_wind+x for x in xrange(num_forecast_winds)]
+				forecast_wind = forecast_winds[windgraphs.PARSED_WEATHER_CHANNELS.index(channel) % len(forecast_winds)]
+				print "insert into wind_forecasts_parsed values (%10s, %d, '%s', %d, '%s', %d, -1);" % \
+						("'%s'" % channel, str_to_em(time_retrieved_str), time_retrieved_str, str_to_em(target_time_str), target_time_str, 
+								forecast_wind)
 
 
 
