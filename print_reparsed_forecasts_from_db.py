45536096a4d791cc7ac53bd913df9fd37390207e
danieltripp <danieltripp@localhost>
2017-09-01 23:39:55 -0400

    .

--- print_reparsed_forecasts_from_db.py
+++ print_reparsed_forecasts_from_db.py
@@ -5,7 +5,11 @@ import windgraphs
 
 if __name__ == '__main__':
 
-	weather_channel = sys.argv[1]
-	datestr = sys.argv[2]
-	windgraphs.print_reparsed_forecasts_from_db(weather_channel, datestr)
+	if len(sys.argv) != 3:
+		print 'This program needs two arguments: 1) a raw weather channel, and 2) a date/time.'
+		sys.exit(1)
+	else:
+		weather_channel = sys.argv[1]
+		datestr = sys.argv[2]
+		windgraphs.print_reparsed_forecasts_from_db(weather_channel, datestr)
 
