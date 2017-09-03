8c80466bf2a34cbed098841f94e68054d1fcfa5f
danieltripp <danieltripp@localhost>
2017-09-03 13:01:54 -0400

    .

--- reparse_raw_forecasts_in_db.py
+++ reparse_raw_forecasts_in_db.py
@@ -6,11 +6,13 @@ from dtpythonutil.misc import *
 
 if __name__ == '__main__':
 
-	weather_channel = sys.argv[1]
-	if len(sys.argv) in (3, 4):
+	if len(sys.argv) not in (3, 4):
+		print 'This program needs either 2 or 3 arguments:'
+		print '1) a raw weather channel, 2) a start date/time, and 3) (optional) an end date/time.'
+		sys.exit(1)
+	else:
+		weather_channel = sys.argv[1]
 		start_date_incl = str_to_em(sys.argv[2])
 		end_date_excl = str_to_em(sys.argv[3]) if len(sys.argv) == 4 else now_em()
 		windgraphs.reparse_raw_forecasts_in_db(weather_channel, start_date_incl, end_date_excl)
-	else:
-		raise Exception()
 
